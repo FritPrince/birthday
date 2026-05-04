7cfeb201e14ddd5ca44f42b0ea57843727e77fe9
Prince <aprinceaineel@gmail.com>
2026-05-05 01:57:52 +0200

    build fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    outputFileTracingRoot: __dirname,
+  },
 };
 
 export default nextConfig;
